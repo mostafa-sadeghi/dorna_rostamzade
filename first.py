d62c0dc8b28cca03fdae3a7b552a60e2197f9737
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-01-07 17:23:44 +0330

    first commit

---
+++ first.py
@@ -0,0 +1,19 @@
+# print('hello world')
+# print('this is our first program in python')
+# print(1)
+
+# print(1+2)
+# print('1+2')
+# print(1 + 2 * 3)
+# print((1+2)*3)
+
+# () * / + -
+
+# print(1 + 2 * 4 / 2)
+# how to define a variable
+x = 0
+n = ''
+name = 'nikan'
+print(x)
+print(n)
+print(name)
