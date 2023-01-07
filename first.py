81f476ad8b2805f501d7a8880278ab2a4b0149d6
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-01-07 17:55:34 +0330

    17dey

--- first.py
+++ first.py
@@ -11,9 +11,45 @@
 
 # print(1 + 2 * 4 / 2)
 # how to define a variable
-x = 0
-n = ''
-name = 'nikan'
-print(x)
-print(n)
-print(name)
+# x = 0
+# n = ''
+# name = 'nikan'
+# print(x)
+# print(n)
+# print(name)
+
+# number1 = 12
+# number2 = 13
+# number = number1 + number2
+# print(number)
+
+# variable name convention :
+# 1. characters + numbers + _    هر تر کیبی از حروف، اعداد و _
+# _n, n_, n1, name1, name_1  # قابل قبول
+
+# 6n, n-1, n@w  # غیر قابل
+
+# data types:
+# numbers -> int : 1,2,3,-1,0      -     float:  1.2
+# str -> 'hi'
+# bool -> True or False
+
+
+# x = 1
+# print(type(x))  # نوع متعیر را مشخص می کنم
+# x = 1.2
+# print(type(x))
+# x = 'hi'
+# print(type(x))
+# x = True
+# print(type(x))
+# x = False
+# print(type(x))
+
+
+message1 = "hi every body. here is python class.\nwe are going to learn every thing about python.\npython is very poweful language"
+print(message1)
+message2 = '''hi every body. here is python class. 
+we are going to learn every thing about python. 
+python is very poweful language'''
+print(message2)
