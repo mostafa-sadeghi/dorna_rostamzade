594bb85e872d3aa344c4f51eb1241e86a2d2318e
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-01-23 17:28:42 +0330

    3bahman

---
+++ about_dict.py
@@ -0,0 +1,57 @@
+# about dictionary
+
+# favorite_sports = []
+# favorite_sports.append("tennis")
+# favorite_sports.append("baseball")
+
+# print(favorite_sports)
+
+# favorite_sports = {}
+# print(type(favorite_sports))
+
+# favorite_sports['dorna'] = "tennis"
+# favorite_sports['nikan'] = "football"
+# favorite_sports['vihan'] = "socer"
+# favorite_sports['rozhman'] = "baseball"
+
+# print(favorite_sports)
+
+
+# favorite_sports = {
+#     'dorna': 'tennis',
+#     'nikan': 'footbal',
+#     'ali': 'baseball'
+# }
+
+
+# print('dorna likes', favorite_sports['dorna'])
+# print('nikan likes', favorite_sports['nikan'])
+
+# casting
+
+# x = 1 + '1' = error
+
+# number = str(1)
+# output = 'a' + number
+# print(output)
+
+# char = '1'
+# number = int(char)
+# print(1 + number)
+
+# exercises
+
+'''
+string = '123456789'
+1. slice output : '345'
+
+2. جمع عددهای موجود در string
+
+3. from string print : 13579
+
+
+'''
+string = '123456789'
+print(int(string[0]) + int(string[1]))
+print(string[1])
+print(string[::2])
