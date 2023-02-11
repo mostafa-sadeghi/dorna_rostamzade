ae5d578db2ea8bcd9f5c5f979117d3418e486f80
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-02-11 17:58:57 +0330

    sadeghi

---
+++ about_loop.py
@@ -0,0 +1,20 @@
+# for loop
+
+string = 'abcdefghi'
+print("each char below")
+# print(string[0])
+# print(string[1])
+# print(string[2])
+# print(string[3])
+# print(string[4])
+# print(string[5])
+# print(string[6])
+# print(string[7])
+# print(string[8])
+for s in string:
+    print(s)
+print('each char after with space')
+# print(string[0], string[1], string[2], string[3])
+for s in string:
+    print(s, end=' ')
+# exercise : write a program that gets a name from input and prints each char's of the name with - separator
---
+++ condition_blocks.py
@@ -0,0 +1,51 @@
+# name = input("enter your name: ")
+# family = input('enter your family: ')
+# age = int(input('enter your age: '))
+
+
+# if age >= 18:
+#     print("you can submit.")
+#     print()
+#     print("ok")
+# else:
+#     print("you are'nt old enough to register.")
+
+# >=   <=    >    <    ==     !=
+
+# entry = input('enter exit |  ok   |    some thing else ')
+# if entry == 'exit':
+#     exit()
+# elif entry == 'ok':
+#     print("ok")
+# else:
+#     print("some thing ...")
+
+
+# exercise : نمرات سه درس پایتون، اسکرچ و اپ اینونتر یک دانش آموز رو بگیری و معدلش رو نشون بدی
+# float type
+# اگر معدل دانش آموز بزرگتر از 19 بود، A
+# اگر معدل دانش آموز بزرگتر از 18 بود B
+# اگر معدل دانش آموز بزرگتر از 17 بود، c
+# اگر معدل دانش آموز بزرگتر از 16 بود D
+# در غیر اینصورت E
+
+
+# name = input('enter your name: ')
+# age = int(input('enter your age: '))  # casting or converting str type to int
+
+# if age <= 8:
+#     print(f'{name} is kid.')
+
+# elif age > 8 and age <= 18:
+#     print(f'{name} is junior.')
+# else:
+#     print(f'{name} is adult.')
+
+
+number = int(input('enter a number> '))
+
+if number == 11 or number == 13:
+    print("ok")
+
+else:
+    print("Not Ok")
