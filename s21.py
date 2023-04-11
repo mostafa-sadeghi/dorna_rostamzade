3dc9b861d15a4b4e9f1bfd6ba72b15fd937b241a
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-04-11 21:56:01 +0330

    sadeghi

---
+++ s21.py
@@ -0,0 +1,79 @@
+# for i in range(5):
+#     print("I want to learn pygame")
+#     print("       ")
+
+# for i in range(1,11):
+#     print(i)
+
+# for i in range(2,11,2):
+#     print(i, end=' ')
+
+# print()
+
+# for i in range(1,10,2):
+#     print(i, end=' ')
+
+
+# for i in range(10,0,-1):
+#     print(i)
+# for index,s in enumerate("dorna"):
+#     # print(s[0], s[1])
+#     print(index, s)
+# for index, number in enumerate([1,2,3,4,5,6,7,8,9]):
+#     print(f'{index}   =>    {number}')
+# for index, number in enumerate((1,2,3,4,5,6,7,8,9)):
+#     print(f'{index}   =>    {number}')
+# student = {
+#     'id': 1,
+#     'name': 'dorna',
+#     'age': 12, 
+#     'grade': 'A'
+# }
+# for item in student:
+#     print(item)
+# for value in student.values():
+#     print(value)
+# for key,value in student.items():
+#     print(key,value)
+
+
+# for i in range(3):
+#     print("a")
+
+
+# for j in range(3):
+#     print("b")
+
+
+# for i in range(3):
+#     print("a")
+#     for j in range(3):
+#         print("b")
+
+# numbers = [2,1,3]
+
+# for i in range(len(numbers)): # i = 0  ......    8
+#     for j in range(i+1,len(numbers)): # j = 0  ......   8
+#         if numbers[i]>numbers[j]: # 
+#             break
+
+#         else:
+#             print("no")
+
+
+total = 0
+
+for i in range(5):
+    new_number = int(input('enter a number:'))
+    total += new_number
+
+print("the total is :", total)
+
+
+number_of_zeros = 0
+for i in range(5):
+    number = int(input('enter a number: '))
+    if number == 0:
+        number_of_zeros += 1
+
+print("number of zero is:", number_of_zeros)
