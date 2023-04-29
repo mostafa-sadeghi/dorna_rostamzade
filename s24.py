085988f5ba19cbf8a6279f2e2b7fe100b3fb47e8
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-04-29 17:56:20 +0330

    sadeghi

---
+++ s24.py
@@ -0,0 +1,37 @@
+import pygame
+pygame.init()
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+size = (700, 500)
+screen = pygame.display.set_mode(size)
+# width = screen.get_width()
+# height = screen.get_height()
+# print(f"width : {width}, height : {height}")
+xpos = 0
+ypos = 0
+change_x_val = 1
+change_y_val = 1
+clock = pygame.time.Clock()
+done = True
+while done:
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                done = False
+
+        elif event.type == pygame.QUIT:
+            done = False
+    screen.fill(BLACK)
+    pygame.draw.rect(screen, WHITE, [xpos, ypos, 10, 10])
+    xpos += change_x_val
+    ypos += change_y_val
+
+    if ypos > 490 or ypos < 0:
+        change_y_val = -1 * change_y_val
+
+    if xpos > 690 or xpos < 0:
+        change_x_val = -1 * change_x_val
+
+    pygame.display.flip()
+    clock.tick(60)
