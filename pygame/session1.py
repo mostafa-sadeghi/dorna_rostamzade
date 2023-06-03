ff5c1645407015c8dd4c9e884bf7bcb730c28b51
msa <msai.sadehi@gmail.com>
2023-06-03 17:59:08 +0330

    sadeghi

--- pygame/session1.py
+++ pygame/session1.py
@@ -19,16 +19,21 @@ DARKGREEN = (10,50,10)# RGB
 BLACK = (0,0,0)# RGB
 WHITE = (255,255,255)# RGB
 
-sound_1 = pygame.mixer.Sound('sound_1.wav')
-sound_1.play()
-pygame.time.delay(500)
-sound_2 = pygame.mixer.Sound('sound_2.wav')
-sound_2.play()
-pygame.time.delay(500)
+# sound_1 = pygame.mixer.Sound('sound_1.wav')
+# sound_1.play()
+# pygame.time.delay(500)
+# sound_2 = pygame.mixer.Sound('sound_2.wav')
+# sound_2.play()
+# pygame.time.delay(500)
 
-sound_2.set_volume(0.1)
-sound_2.play()
+# sound_2.set_volume(0.1)
+# sound_2.play()
 
+pygame.mixer.music.load('music.wav')
+
+pygame.mixer.music.play(-1,0.0)
+# pygame.time.delay(3000)
+# pygame.mixer.music.stop()
 
 fonts = pygame.font.get_fonts()
 # for font in fonts:
@@ -58,17 +63,32 @@ dragon_right_image = pygame.image.load("dragon_right.png")
 dragon_right_rect = dragon_right_image.get_rect()
 dragon_right_rect.topright = (WINDOW_WIDTH, 0)
 
-pygame.draw.line(display_surface, WHITE, (0,75), (WINDOW_WIDTH, 75), 4)
+
+
+dragon_image = pygame.image.load('dragon_right.png')
+dragon_rect = dragon_image.get_rect()
+dragon_rect.centerx = WINDOW_WIDTH//2
+dragon_rect.bottom = WINDOW_HEIGHT
+
+
+
+VELOCITY = 30
+
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                dragon_rect.x -= VELOCITY
 
+    display_surface.fill(BLACK)
     display_surface.blit(dragon_left_image, dragon_left_rect)
     display_surface.blit(dragon_right_image, dragon_right_rect)
+    pygame.draw.line(display_surface, WHITE, (0,75), (WINDOW_WIDTH, 75), 4)
     # display_surface.blit(system_text,system_text_rect)
-
+    display_surface.blit(dragon_image,dragon_rect)
     # pygame.draw.line() # exercise  کشیدن خط سفید زیر دو دراگون
 
 
